07f8b3e7798e3272eeb5655b72efe29dfea7dd9c
Erik Zettersten <Erik@Zettersten.com>
2016-06-02 18:32:06 -0400

    updates files

--- src/jquery.hosted-payments.js
+++ src/jquery.hosted-payments.js
@@ -22,51 +22,51 @@
 // Closure
 (function(){
 
-	/**
-	 * Decimal adjustment of a number.
-	 *
-	 * @param	{String}	type	The type of adjustment.
-	 * @param	{Number}	value	The number.
-	 * @param	{Integer}	exp		The exponent (the 10 logarithm of the adjustment base).
-	 * @returns	{Number}			The adjusted value.
-	 */
-	function decimalAdjust(type, value, exp) {
-		// If the exp is undefined or zero...
-		if (typeof exp === 'undefined' || +exp === 0) {
-			return Math[type](value);
-		}
-		value = +value;
-		exp = +exp;
-		// If the value is not a number or the exp is not an integer...
-		if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
-			return NaN;
-		}
-		// Shift
-		value = value.toString().split('e');
-		value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
-		// Shift back
-		value = value.toString().split('e');
-		return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-	}
+    /**
+     * Decimal adjustment of a number.
+     *
+     * @param   {String}    type    The type of adjustment.
+     * @param   {Number}    value   The number.
+     * @param   {Integer}   exp     The exponent (the 10 logarithm of the adjustment base).
+     * @returns {Number}            The adjusted value.
+     */
+    function decimalAdjust(type, value, exp) {
+        // If the exp is undefined or zero...
+        if (typeof exp === 'undefined' || +exp === 0) {
+            return Math[type](value);
+        }
+        value = +value;
+        exp = +exp;
+        // If the value is not a number or the exp is not an integer...
+        if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+            return NaN;
+        }
+        // Shift
+        value = value.toString().split('e');
+        value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+        // Shift back
+        value = value.toString().split('e');
+        return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+    }
 
-	// Decimal round
-	if (!Math.round10) {
-		Math.round10 = function(value, exp) {
-			return decimalAdjust('round', value, exp);
-		};
-	}
-	// Decimal floor
-	if (!Math.floor10) {
-		Math.floor10 = function(value, exp) {
-			return decimalAdjust('floor', value, exp);
-		};
-	}
-	// Decimal ceil
-	if (!Math.ceil10) {
-		Math.ceil10 = function(value, exp) {
-			return decimalAdjust('ceil', value, exp);
-		};
-	}
+    // Decimal round
+    if (!Math.round10) {
+        Math.round10 = function(value, exp) {
+            return decimalAdjust('round', value, exp);
+        };
+    }
+    // Decimal floor
+    if (!Math.floor10) {
+        Math.floor10 = function(value, exp) {
+            return decimalAdjust('floor', value, exp);
+        };
+    }
+    // Decimal ceil
+    if (!Math.ceil10) {
+        Math.ceil10 = function(value, exp) {
+            return decimalAdjust('ceil', value, exp);
+        };
+    }
 
 })();
 /* jquery.signalR.core.js */
@@ -4716,27 +4716,27 @@
 })(jQuery, window, document);
 
 (function($, window, document, undefined) {
-	
-	"use strict";
+    
+    "use strict";
 
     /*
      * Export "hp"
      */
     window.hp = hp || {};
 
-	function Success($element) {
+    function Success($element) {
         this.context = null;
         this.$parent = null;
         this.$content = null;
-    	this.$element = $element;
+        this.$element = $element;
         this.formData = { _isValid: false };
     }
 
-	Success.prototype.init = function () { 
+    Success.prototype.init = function () { 
 
         var context = hp.Utils.handleLegacyCssClassApplication("success", this.$element),
-        	$parent = context.parent,
-        	$content = context.content;
+            $parent = context.parent,
+            $content = context.content;
 
         $parent
             .find(".icon.success")
@@ -4747,19 +4747,19 @@
         this.$parent = $parent;
         this.$content = $content;
 
-	};
+    };
 
-	Success.prototype.createTemplate = function() {
+    Success.prototype.createTemplate = function() {
 
-		var $html = [
-			'<div class="hp-success-visual"></div>',
-			'<h2>{{successLabel}}</h2>',
-			'<p class="text-muted">{{redirectLabel}}</p>'
-		].join("");
+        var $html = [
+            '<div class="hp-success-visual"></div>',
+            '<h2>{{successLabel}}</h2>',
+            '<p class="text-muted">{{redirectLabel}}</p>'
+        ].join("");
 
-		return $html;
+        return $html;
 
-	};
+    };
 
     Success.prototype.showSuccess = function(delay) {
         return hp.Utils.showSuccessPage(delay);
@@ -7799,7 +7799,7 @@
     var pluginName = "hp",
         defaults = {};
 
-    defaults.version = "v3.2.0";
+    defaults.version = "v3";
     defaults.amount = 0;
     defaults.baseUrl = "https://www.etsemoney.com/hp/v3/adapters";
     defaults.defaultCardCharacters = "&middot;&middot;&middot;&middot; &middot;&middot;&middot;&middot; &middot;&middot;&middot;&middot; &middot;&middot;&middot;&middot;";
