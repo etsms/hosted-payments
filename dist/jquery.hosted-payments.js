a3390f3fceb7ccd5388ffbee6206c27b82f38e57
Erik Zettersten <Erik@Zettersten.com>
2017-10-02 15:46:27 -0400

    vbump

--- dist/jquery.hosted-payments.js
+++ dist/jquery.hosted-payments.js
@@ -10811,7 +10811,7 @@
 
 }).call(this);
 /*
- *  jQuery Hosted Payments - v3.8.32
+ *  jQuery Hosted Payments - v3.8.33
  *
  *  Made by Erik Zettersten
  *  Under MIT License
@@ -10821,7 +10821,7 @@
     var pluginName = "hp",
         defaults = {};
 
-    defaults.version = "v3.8.32";
+    defaults.version = "v3.8.33";
     defaults.amount = 0;
     defaults.baseUrl = "https://htv.emoney.com/v3/adapters";
     defaults.defaultCardCharacters = "&middot;&middot;&middot;&middot; &middot;&middot;&middot;&middot; &middot;&middot;&middot;&middot; &middot;&middot;&middot;&middot;";
