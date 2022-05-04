8d9c65c5d3d6cc0a1df372b9b2b73a2547acced4
Kelly Quinn <kssmith92@gmail.com>
2022-05-04 10:49:52 -0400

    - ETS-396 allowing only numbers and alphabet in zip code

--- dist/jquery.hosted-payments.js
+++ dist/jquery.hosted-payments.js
@@ -4819,7 +4819,22 @@
         '<div class="hp-pull-left hp-no-margins">',
         '<label class="hp-label-avs" for="avsZip">Zip <span class="hp-avs-required">*</span></label>',
         '<div class="hp-input hp-input-avs hp-input-avs-zip">',
-        '<input maxlength="5" pattern="\\d*" placeholder="Zipcode" value="' + hp.Utils.defaults.billingAddress.postalCode + '" name="avsZip" id="avsZip" autocomplete="' + (hp.Utils.defaults.disableAutocomplete ? "off" : "postal-code") + '" type="text">',
+
+
+
+            Skip to content
+Using Gmail with screen readers
+        Meet
+        New meeting
+        Join a meeting
+        Hangouts
+
+        Conversations
+        14.47 GB of 100 GB used
+        Terms · Privacy · Program Policies
+        Last account activity: 1 hour ago
+        Details
+        '<input maxlength="5" pattern="[^0-9a-zA-Z]+" placeholder="Zipcode" value="' + hp.Utils.defaults.billingAddress.postalCode + '" name="avsZip" id="avsZip" autocomplete="' + (hp.Utils.defaults.disableAutocomplete ? "off" : "postal-code") + '" type="text">',
         "</div>",
         "</div>",
         "</div>",
